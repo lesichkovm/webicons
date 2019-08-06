b48867baf1851bba2bd28a1b76abe089e7e373a8
Milan Lesichkov <lesichkovm@gmail.com>
2019-08-06 18:23:48 +0100

    Create typicons.php

---
+++ tools/typicons.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$folderPath = 'C:\Users\MILAN\Downloads\typicons.font-master/src/svg';
+$stub = "var iconsets = iconsets || {}; iconsets.typicons = iconsets.typicons || {};";
+
+$filepath = __DIR__ . '/ty.js';
+file_put_contents($filepath, $stub . "\n");
+
+
+$files = \Sinevia\FileUtils::directoryListFiles($folderPath);
+$data = [];
+foreach ($files as $file) {
+    $ext = pathinfo($file, PATHINFO_EXTENSION);
+    $filename = pathinfo($file, PATHINFO_FILENAME);
+    if (in_array($ext, ['svg']) == false) {
+        continue;
+    }
+    $contents = file_get_contents($file);
+    $base64_contents = base64_encode($contents);
+    if ($ext == 'svg') {
+        $base64_imagedata = 'data:image/svg+xml;base64,' . $base64_contents;
+    }
+    $data[$filename] = $base64_imagedata;
+}
+// DEBUG: var_dump($data);
+$jsString = '';
+foreach ($data as $icon => $data) {
+    $jsString .= "iconsets.typicons['" . $icon . "'] = '$data';\n";
+}
+// echo $jsString;
+file_put_contents($filepath, $jsString . "\n", FILE_APPEND);
